7934128d24d24e49f83dcc9f544b6f460b9e9d59
mathaz3 <mathaz3@tuta.io>
2025-04-15 17:29:25 -0300

    Update CF.py

--- CF.py
+++ CF.py
@@ -5,25 +5,18 @@ from os import system
 def tentativa():
     global formatos
 
-    # nao achei outra maneira de fazer isso, no futuro talvez eu melhore isso
     varia = ""
-    varia += choice(ascii_lowercase+digits)
-    varia += choice(ascii_lowercase+digits)
-    varia += choice(ascii_lowercase+digits)
-    varia += choice(ascii_lowercase+digits)
-    varia += choice(ascii_lowercase+digits)
-    varia += choice(ascii_lowercase+digits)
+    for x in range(6):
+        varia += choice(ascii_lowercase+digits)
 
     for formato in formatos:
         url = f"https://files.catbox.moe/{varia}.{formato}"
         print(f"testando {url}")
         system(f"wget {url}")
         
-
-
 formatos = input("quais formatos procurar(separados por virgula)?: ").split(",")
 if formatos == [""]:
-    formatos = ["jpg","png","gif","mp4","avi","txt"]
+    formatos = ["jpg","png","mp4"]
 
 print("tentar quantas vezes?: ")
 opcao=int(input("1---limitadas | 2---infinitas\n---> "))
